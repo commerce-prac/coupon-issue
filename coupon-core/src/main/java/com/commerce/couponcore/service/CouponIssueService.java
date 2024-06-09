c49c843aa244952bf7f0e9f354e2baafd1e4d221
Shinnybest <kim295521@naver.com>
2024-06-09 13:11:58 +0900

    [ISSUE-007] coupon 발급 메서드 작성 -- - 쿠폰 발급 시, 쿠폰 존재 여부 확인 - 쿠폰 발급 - 쿠폰 발급 기록 저장   - 쿠폰 발급 기록 저장 시, 이미 발급된 사용자인지 체크

---
+++ coupon-core/src/main/java/com/commerce/couponcore/service/CouponIssueService.java
@@ -0,0 +1,52 @@
+package com.commerce.couponcore.service;
+
+import com.commerce.couponcore.exception.CouponIssueException;
+import com.commerce.couponcore.exception.ErrorCode;
+import com.commerce.couponcore.model.Coupon;
+import com.commerce.couponcore.model.CouponIssue;
+import com.commerce.couponcore.repository.CouponIssueJpaRepository;
+import com.commerce.couponcore.repository.CouponIssueRepository;
+import com.commerce.couponcore.repository.CouponJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CouponIssueService {
+
+    private final CouponJpaRepository couponJpaRepository;
+    private final CouponIssueJpaRepository couponIssueJpaRepository;
+    private final CouponIssueRepository couponIssueRepository;
+
+    @Transactional
+    public void issue(long couponId, long userId) {
+        Coupon coupon = findCoupon(couponId);
+        coupon.issue();
+        saveCouponIssue(couponId, userId);
+    }
+
+    @Transactional
+    public CouponIssue saveCouponIssue(long couponId, long userId) {
+        checkAlreadyIssued(couponId, userId);
+        CouponIssue couponIssue = CouponIssue.builder()
+                .couponId(couponId)
+                .userId(userId)
+                .build();
+        return couponIssueJpaRepository.save(couponIssue);
+    }
+
+    @Transactional(readOnly = true)
+    public void checkAlreadyIssued(long couponId, long userId) {
+        CouponIssue couponIssue = couponIssueRepository.findFirstCouponIssue(couponId, userId);
+        if (couponIssue != null) {
+            throw new CouponIssueException(ErrorCode.COUPON_DUPLICATE);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Coupon findCoupon(long couponId) {
+        return couponJpaRepository.findById(couponId)
+                .orElseThrow(() -> new CouponIssueException(ErrorCode.COUPON_NOT_EXIST));
+    }
+}

83d60ae3529ee8e9fcdf1568d906eaa72b60ff70
Shinnybest <kim295521@naver.com>
2024-06-27 14:13:42 +0900

    [ISSUE-#16] RedisTemplate에 String 키, 값을 직렬화하는 설정 추가

--- coupon-core/src/main/java/com/commerce/couponcore/config/RedisConfig.java
+++ coupon-core/src/main/java/com/commerce/couponcore/config/RedisConfig.java
@@ -3,9 +3,12 @@ package com.commerce.couponcore.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -17,11 +20,24 @@ public class RedisConfig {
     private int port;
 
     @Bean
-    RedissonClient redissonClient() {
+    public RedissonClient redissonClient() {
         Config config = new Config();
         String address = "redis://" + host + ":" + port;
         config.useSingleServer().setAddress(address);
         return Redisson.create(config);
     }
 
+    @Bean
+    public RedissonConnectionFactory redissonConnectionFactory() {
+        return new RedissonConnectionFactory(redissonClient());
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redissonConnectionFactory());
+        return redisTemplate;
+    }
 }
